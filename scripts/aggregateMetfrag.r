08e4d9784bdba896f3cf788e38b99806198e4542
payamemami <payam.emami@medsci.uu.se>
2017-07-19 09:53:20 +0200

    create the container

---
+++ scripts/aggregateMetfrag.r
@@ -0,0 +1,68 @@
+#!/usr/bin/env Rscript
+
+options(stringAsfactors = FALSE, useFancyQuotes = FALSE)
+
+# Taking the command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+if(length(args)==0)stop("No file has been specified!")
+
+output<-NA
+appendTo<-NA
+appendToName<-"realNames"
+cleanFileName=T
+for(arg in args)
+{
+  argCase<-strsplit(x = arg,split = "=")[[1]][1]
+  value<-strsplit(x = arg,split = "=")[[1]][2]
+  
+  if(argCase=="realNames")
+  {
+    realNames=as.character(value)
+  }
+  if(argCase=="inputs")
+  {
+    inputs=as.character(value)
+  }
+
+  if(argCase=="output")
+  {
+    output=as.character(value)
+  }
+  
+}
+
+
+inputs<-gsub(pattern = " ",replacement = "",strsplit(x = inputs,split = ",",fixed=T)[[1]],fixed=T)
+realNamesTMP<-gsub(pattern = " ",replacement = "",strsplit(x = realNames,split = ",",fixed=T)[[1]],fixed=T)
+
+inputs<-inputs[inputs!=""]
+realNamesTMP<-realNamesTMP[realNamesTMP!=""]
+
+
+if(cleanFileName)
+  realNamesTMP<-gsub(pattern = "Galaxy.*-\\[|\\].*",replacement = "",x = realNamesTMP)
+
+allMS2IDs<-c()
+for(i in 1:length(inputs))
+{
+  # check if the file is empty
+  info = file.info(inputs[i])
+  if(info$size!=0 & !is.na(info$size))
+  {
+    tmpFile<-read.csv(inputs[i])
+    
+    # Extract mz and rt from the real file names
+    rt<-as.numeric(strsplit(x = realNamesTMP[i],split = "_",fixed = T)[[1]][2])
+    mz<-as.numeric(strsplit(x = realNamesTMP[i],split = "_",fixed = T)[[1]][3])
+
+    allMS2IDs<-rbind(allMS2IDs,data.frame(parentMZ=mz,parentRT=rt,tmpFile))
+  }
+
+}
+
+
+write.csv(x = allMS2IDs,file = output)
+
+
+
